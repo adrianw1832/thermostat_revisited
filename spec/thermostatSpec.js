d01b777ae57d53cd1580e77563e11971ea83a1ea
Adrian Wong <awong@thoughtworks.com>
2016-10-23 20:21:31 +0100

    Add power saving mode and maximum temperature for thermostat

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -29,4 +29,36 @@ describe('thermostat', () => {
     }
     expect(thermostat.defaultTemperature).toEqual(10);
   });
+
+  it('has a default power saving mode off', () => {
+    expect(thermostat.powerSavingMode).toBe(false);
+  });
+
+  describe('#powerSavingMode', () => {
+    it('toggles to on when it is called once', () => {
+      thermostat.togglePowerSavingMode();
+      expect(thermostat.powerSavingMode).toBe(true);
+    });
+
+    it('toggles to off when it is called twice', () => {
+      thermostat.togglePowerSavingMode();
+      thermostat.togglePowerSavingMode();
+      expect(thermostat.powerSavingMode).toBe(false);
+    });
+  });
+
+  it('has a maximum temperature of 35 when powerSavingMode is off', () => {
+    for (let i = 0; i < 16; i++) {
+      thermostat.increaseTemperature();
+    }
+    expect(thermostat.defaultTemperature).toEqual(35);
+  });
+
+  it('has a maximum temperature of 25 when powerSavingMode is on', () => {
+    thermostat.togglePowerSavingMode();
+    for (var i = 0; i < 6; i++) {
+      thermostat.increaseTemperature();
+    }
+    expect(thermostat.defaultTemperature).toEqual(25);
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,14 +1,27 @@
 let Thermostat = function Thermostat() {
   this.defaultTemperature = 20;
   this.minimumTemperature = 10;
+  this.maximumTemperature = 35;
+  this.powerSavingMode = false;
 };
 
 Thermostat.prototype.increaseTemperature = function() {
-  this.defaultTemperature += 1;
+  if (this.defaultTemperature < this.maximumTemperature) {
+    this.defaultTemperature += 1;
+  }
 };
 
 Thermostat.prototype.decreaseTemperature = function() {
   if (this.defaultTemperature > this.minimumTemperature) {
     this.defaultTemperature -= 1;
+  }
+};
+
+Thermostat.prototype.togglePowerSavingMode = function() {
+  this.powerSavingMode = !this.powerSavingMode;
+  if (this.powerSavingMode) {
+    this.maximumTemperature = 25;
+  } else {
+    this.maximumTemperature = 35;
   }
 };
